8bfa8a45078b86adb6d60f3786cc0e7f3c36dcc6
Katherine Go <katiego@dhcp-clara-3426.eduroam.cornell.edu>
2018-11-26 16:48:01 -0500

    Initial Commit

---
+++ cornellcreatives/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cornellcreatives
+//
+//  Created by Katherine Go on 11/26/18.
+//  Copyright © 2018 Katherine Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
